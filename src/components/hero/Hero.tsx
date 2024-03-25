5e3c7132d1e42ad7e65326b1a4e2d18031a5f430
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-03-25 23:44:36 +0600

    update dc doe

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Flex, Grid, Spacer } from '@chakra-ui/react';
+import { Flex, FlexProps, Grid, Spacer } from '@chakra-ui/react';
 import { BASE_PADDING } from '@/lib/constants';
 import ProfileImage from './ProfileImage';
 import HeroText from './HeroText';
 
-type HeroProps = {};
+type HeroProps = FlexProps & {};
 
-const Hero: React.FC<HeroProps> = ({}) => {
+const Hero: React.FC<HeroProps> = ({ ...props }) => {
 	return (
-		<Flex flexDir='column' h='100vh' px={BASE_PADDING}>
+		<Flex flexDir='column' h='100vh' px={BASE_PADDING} {...props}>
 			<Grid
 				flex={{ base: 0, lg: 1 }}
 				templateColumns={{ base: '1fr', lg: '1fr 1fr' }}
