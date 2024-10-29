1dea32f5957433e5514c4bb42075c32e552320a5
Mubin42 <kazimubin.46@gmail.com>
2024-10-29 23:35:02 +0600

    feat: created desktop navigation

---
+++ src/components/navbar/DesktopNavItem.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { FC } from "react";
+
+type Props = {
+  data: NavData;
+  className?: string;
+};
+
+const DesktopNavItem: FC<Props> = ({ data, className }) => {
+  return (
+    <Link href={data.href}>
+      <div
+        className={cn(
+          "relative text-md transition duration-150 ease-in-out hover:-translate-y-1",
+          className,
+        )}
+      >
+        <p className="relative after:absolute after:bottom-[-4px] after:left-0 after:h-[4px] after:w-0 after:bg-current after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+          {data.title}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default DesktopNavItem;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,33 @@
 "use client";
 import { FC } from "react";
 import ModeToggle from "../custom-components/ModeToggle";
+import navData from "./navData";
+import Link from "next/link";
+import { Button } from "../neobrutalism-ui/Button";
+import DesktopNavItem from "./DesktopNavItem";
 
 type Props = {};
 
 const Navbar: FC<Props> = ({}) => {
+  const desktopRight = (
+    <div className="hidden md:block">
+      <div className="flex-1 flex items-center gap-14 max-lg:hidden">
+        {navData.map((nav, index) => {
+          return <DesktopNavItem key={index} data={nav} />;
+        })}
+        <Button>
+          <a href="/kazi_mubin_cv.pdf" download>
+            <span>Resume</span>
+          </a>
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="flex w-full h-16 bg-navbar px-4 lg:px-32 justify-between items-center">
       <ModeToggle />
+      {desktopRight}
     </div>
   );
 };
