045915fecaa644ee60c6ec04681dcd8e20ca2eb6
Mubin42 <kazimubin.46@gmail.com>
2024-10-29 20:54:00 +0600

    feat: setup dark mode in shade cn

---
+++ src/components/providers/ThemeProvider.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+import React, { FC } from "react";
+
+const ThemeProvider: FC<ThemeProviderProps> = ({ children, ...props }) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
+
+export default ThemeProvider;
