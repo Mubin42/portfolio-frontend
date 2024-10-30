026658b5ab531758f2b65963963adc4a0ad9f011
Mubin42 <kazimubin.46@gmail.com>
2024-10-31 01:11:58 +0600

    feat: lennis installed for smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,56 @@
 "use client";
 
 import Hero from "@/components/custom-components/Hero";
+import { ExternalLinkIcon } from "lucide-react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
+  const stacks = (
+    <article className="space-y-6">
+      <p className="font-cera font-bold">
+        Here are a few technologies that I&apos;ve been working with recently:
+      </p>
+      <div className="flex justify-between md:justify-start">
+        <ul className="space-y-2 md:mr-16">
+          <li>&rarr; Javascript</li>
+          <li>&rarr; Typescript</li>
+          <li>→ Tailwind</li>
+          <li>→ NodeJS</li>
+        </ul>
+        <ul className="space-y-2">
+          <li>&rarr; ExpressJS</li>
+          <li>&rarr; Docker</li>
+          <li>&rarr; Git / Github</li>
+          <li>&rarr; MongoDB</li>
+        </ul>
+      </div>
+      <p className="font-cera font-bold mb-5">Here&apos;s my social links:</p>
+      <div className="flex justify-between md:justify-start">
+        <p className="flex md:w-32 items-center">
+          <a
+            href="https://www.facebook.com/iamrortiz/"
+            target="_blank"
+            className="flex items-center"
+          >
+            Facebook <ExternalLinkIcon className="ml-2 h-4 w-4" />
+          </a>
+        </p>
+        <p className="flex items-center">
+          <a
+            href="https://www.linkedin.com/in/ralphortiz/"
+            target="_blank"
+            className="flex md:w-32  items-center"
+          >
+            Linkedin <ExternalLinkIcon className="ml-2 h-4 w-4" />
+          </a>
+        </p>
+      </div>
+    </article>
+  );
   return (
-    <div className="grid grid-cols-1">
+    <div className="grid grid-cols-1 gap-4">
       <Hero />
-      <div className="flex min-h-[1000px] bg-red-300">Hello</div>
+      {stacks}
     </div>
   );
 };
--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { FC } from "react";
 import Navbar from "../navbar/Navbar";
 import { cn } from "@/lib/utils";
 import Footer from "../custom-components/Footer";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 type Props = {
   children: React.ReactNode;
@@ -10,18 +12,34 @@ type Props = {
 
 const PageLayout: FC<Props> = ({ children, className }) => {
   return (
-    <div className="bg-background h-screen w-screen">
-      <Navbar />
-      <div
-        className={cn(
-          "px-4 py-32 lg:px-32 lg:py-48 max-w-screen-xl mx-auto",
-          className,
-        )}
-      >
-        {children}
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.1, // Linear interpolation (0-1). Smaller = smoother
+        duration: 1.5, // Scroll duration in seconds
+        smoothWheel: true, // Enable smooth scrolling for mouse wheel
+        // smoothTouch: true, // Don't enable smooth scrolling for touch devices
+        wheelMultiplier: 1, // Wheel speed multiplier
+        infinite: false, // Don't enable infinite scroll
+        orientation: "vertical", // Scroll orientation
+        gestureOrientation: "vertical", // Gesture orientation
+        touchMultiplier: 2, // Touch speed multiplier
+        syncTouch: false, // Enable touch synchronization
+      }}
+    >
+      <div className="bg-background h-screen w-screen">
+        <Navbar />
+        <div
+          className={cn(
+            "px-4 py-32 lg:px-32 lg:py-48 max-w-screen-xl mx-auto",
+            className,
+          )}
+        >
+          {children}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ReactLenis>
   );
 };
 
