fb109237ddede6eb03a223e672849fb1ae6bfd67
Mubin42 <kazimubin.46@gmail.com>
2024-11-17 12:01:26 +0600

    feat: created about section and started card for experience

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import {NextPage} from "next";
-import {RevealLinks} from "@/components/custom-components/RevealLinks";
 
 const AboutPage: NextPage = () => {
-  return <div>
-    <RevealLinks/>
+
+  const description = `I am a Software Engineer with nearly two years of professional software development experience. I have a strong foundation in full-stack web development, with a focus on backend technologies. I am passionate about building high-quality software that solves real-world problems. I am a quick learner and a team player who enjoys collaborating with others to deliver high-quality software.`;
+  return <div className='space-y-4'>
+    <h1 className="text-2xl lg:text-6xl font-black">ABOUT</h1>
+    <div className="space-y-10 text-xl font-medium lg:text-2xl">
+      <p>
+        {description}
+      </p>
+      <p>You can reach me at: <span className='font-bold'>kazimubin.46@gmail.com</span></p>
+    </div>
   </div>;
 };
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,25 @@
-import { NextPage } from "next";
+import {NextPage} from "next";
+import {Card} from "@/components/neobrutalism-ui/Card";
+import {CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/shadcn-ui/Card";
 
 const ProjectPage: NextPage = () => {
-  return <>Project Page</>;
+  return <div className='space-y-4'>
+    <h1 className="text-2xl lg:text-6xl font-black">PROJECTS</h1>
+    <div className='grid grid-cols-1 lg:grid-cols-2'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>Deploy your new project in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          Hello World
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          Foot
+        </CardFooter>
+      </Card>
+    </div>
+  </div>;
 };
 
 export default ProjectPage;
